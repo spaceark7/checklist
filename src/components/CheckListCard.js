2504d16b5565891c71a3703adcfcb1afb25ccd81
SpaceArk7 <spacearkchief@gmail.com>
2021-02-11 12:58:30 +0700

    es

--- src/components/CheckListCard.js
+++ src/components/CheckListCard.js
@@ -2,11 +2,12 @@ import { Card, Row, Col } from 'react-bootstrap'
 
 import { FiEdit, FaTrash } from 'react-icons/all'
 
-const CheckListCard = () => {
+const CheckListCard = ({ data }) => {
+  console.log(data)
   return (
     <Card className='my-3'>
       <Card.Body>
-        <h2> Checklist Name</h2>
+        <h2>{data.name}</h2>
         <div className='container button-container'>
           <Row>
             <Col>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,37 @@
+import axios from 'axios'
 import { Row, Col, Form, Button, Card } from 'react-bootstrap'
 import CheckListCard from './CheckListCard'
 import InputCard from './inputCard'
+import { useEffect, useState } from 'react'
 
 const HomePage = () => {
+  const config = {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('authToken')}`,
+    },
+  }
+
+  const [array, setArray] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://18.139.50.74:8080/checklist', config)
+      .then((res) => {
+        setArray(res.data.data)
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }, [])
+
   return (
     <Row className='d-flex flex-wrap align-items-center my-5'>
       <Col>
         <InputCard />
       </Col>
       <Col>
-        <CheckListCard />
+        {array.map((item) => {
+          return <CheckListCard data={array} />
+        })}
       </Col>
     </Row>
   )
